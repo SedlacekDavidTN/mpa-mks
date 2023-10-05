be814d8370674acef2754f54b7bbae481abde24d
SedlacekDavidTN <xsedla1n@vutbr.cz>
2023-10-05 16:36:12 +0200

    (Ex2) Finished the final excersise

--- Ex2/Core/Src/main.c
+++ Ex2/Core/Src/main.c
@@ -34,6 +34,8 @@
 #define LED_TIME_BLINK 300
 #define LED_TIME_SHORT 100
 #define LED_TIME_LONG 1000
+#define BUTTON_PERIOD_S2 40
+#define BUTTON_PERIOD_S1 5
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -302,26 +304,50 @@ void blinker(void)
 
 void buttons(void) {
 	static uint32_t old_s2;
-	static uint32_t old_s1;
+	// static uint32_t old_s1;
 	static uint32_t off_time;
-	// static uint16_t debounce = 0xFFFF;
+	static uint32_t check_time_1;
+	static uint32_t check_time_2;
+	static uint16_t debounce = 0xFFFF;
 	uint32_t new_s2 = LL_GPIO_IsInputPinSet(S2_GPIO_Port, S2_Pin);
-	uint32_t new_s1 = LL_GPIO_IsInputPinSet(S1_GPIO_Port, S1_Pin);
+	// uint32_t new_s1 = LL_GPIO_IsInputPinSet(S1_GPIO_Port, S1_Pin);
 
-	if (old_s2 && !new_s2) {
-		off_time = Tick + LED_TIME_SHORT;
-		LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	if(Tick > check_time_1 + BUTTON_PERIOD_S2) {
+		check_time_1 = Tick;
+		if (old_s2 && !new_s2) {
+				off_time = Tick + LED_TIME_SHORT;
+				LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+		}
+		old_s2 = new_s2;
 	}
-	old_s2 = new_s2;
 
-	if (old_s1 && !new_s1) {
+	/* if (old_s1 && !new_s1) {
 			off_time = Tick + LED_TIME_LONG;
 			LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
 		}
-	old_s1 = new_s1;
+	old_s1 = new_s1; */
 
 	if (Tick > off_time) {
 		LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	}
+
+	/* if (Tick > check_time_2 + BUTTON_PERIOD_S1) {
+		check_time_2 = Tick;
+		debounce <<= 1;
+		if (LL_GPIO_IsInputPinSet(S1_GPIO_Port, S1_Pin) && 1) debounce |= 0x1;
+		if (debounce == 0x7FFF) {
+			off_time = Tick + LED_TIME_LONG;
+			LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+		}
+	} */
+
+	if (Tick > check_time_2 + BUTTON_PERIOD_S1) {
+		check_time_2 = Tick;
+		debounce = (debounce<<1) + LL_GPIO_IsInputPinSet(S1_GPIO_Port, S1_Pin);
+		if (debounce == 0x7FFF) {
+			off_time = Tick + LED_TIME_LONG;
+			LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+		}
 	}
 }
 /* USER CODE END 4 */
